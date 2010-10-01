7ea1f276f756ccf7019194778812ce2a01fec1e9
David Astels <dastels@Otaria.local>
2010-10-01 11:38:33 -0700

    start filling in implementations

--- Classes/Dsl.m
+++ Classes/Dsl.m
@@ -7,8 +7,157 @@
 //
 
 #import "Dsl.h"
-
+#import "SymbolTable.h"
 
 @implementation Dsl
+
+- (Dsl *) init
+{
+  parser = [[DslParser alloc] init];
+  symbolTable = [[SymbolTable alloc] init];
+  return self;  
+}
+
+
+- (DslExpression *) parse:(NSString*)codeString
+{
+  return [parser parseExpression:[InputStream withString:codeString]];
+}
+
+
+- (DslSymbol *) intern:(NSString *)name
+{
+  return [symbolTable intern:name];
+}
+
+
+- (DslExpression*) bind:(DslSymbol*)symbol to:(DslExpression*)value
+{
+  return [symbolTable bind:symbol to:value];
+}
+
+
+- (DslExpression*) valueOf:(DslSymbol*)symbol
+{
+  return [symbolTable valueOf:symbol];
+}
+
+
+- (void) pushLocalBindings
+{
+  [symbolTable pushLocalBindings];
+}
+
+
+- (void) popLocalBindings
+{
+  [symbolTable popLocalBindings];
+}
+
+
+- (DslFunction*) lambda:(DslCons*)args
+{
+}
+
+
+- (DslFunction*) defun:(DslCons*)args
+{
+}
+
+
+- (DslFunction*) defBuiltin:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) apply:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) let:(DslCons*)args
+{
+}
+
+
+- (DslCons*) cons:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) car:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cdr:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) caar:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cadr:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cdar:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cddr:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) caaar:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) caadr:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cadar:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) caddr:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cdaar:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cdadr:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cddar:(DslCons*)args
+{
+}
+
+
+- (DslExpression*) cdddr:(DslCons*)args
+{
+}
+
+
+- (DslNumber*) length:(DslCons*)args
+{
+}
+
 
 @end
