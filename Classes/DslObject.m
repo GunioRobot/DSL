f1ebc8b124508ce890c095d99c3b07ec6fc2b3f4
David Astels <dastels@Otaria.local>
2010-11-07 01:29:01 -0800

    change DSLObject property manipulation methods to use key value coding

--- Classes/DslObject.m
+++ Classes/DslObject.m
@@ -32,45 +32,51 @@
 
 - (DslNumber*) getInteger:(NSString*)property
 {
-  int intVal = (int)[obj performSelector:sel_getUid([property UTF8String])];
+  int intVal = (int)[obj valueForKey:property];
+//  int intVal = (int)[obj performSelector:sel_getUid([property UTF8String])];
   return [DslNumber numberWith:intVal];
 }
 
 
 - (DslString*) getString:(NSString*)property
 {
-  NSString *strVal = (NSString*)[obj performSelector:sel_getUid([property UTF8String])];
+  NSString *strVal = (NSString*)[obj valueForKey:property];
+//  NSString *strVal = (NSString*)[obj performSelector:sel_getUid([property UTF8String])];
   return [DslString stringWith:strVal];
 }
 
 
 - (DslBoolean*) getBoolean:(NSString*)property
 {
-  BOOL boolVal = (BOOL)[obj performSelector:sel_getUid([property UTF8String])];
+  BOOL boolVal = (BOOL)[obj valueForKey:property];
+//  BOOL boolVal = (BOOL)[obj performSelector:sel_getUid([property UTF8String])];
   return [DslBoolean booleanWith:boolVal];
 }
 
 
 - (DslNumber*) setInteger:(NSString*)property to:(DslNumber*)value
 {
-  NSString *selector = [NSString stringWithFormat:@"%@:", property];
-  int intVal = (int)[obj performSelector:sel_getUid([selector UTF8String]) withObject:[value intValue]];
+  [obj setValue:[value intValue] forKey:property];
+//  NSString *selector = [NSString stringWithFormat:@"%@:", property];
+//  int intVal = (int)[obj performSelector:sel_getUid([selector UTF8String]) withObject:[value intValue]];
   return value;
 }
 
 
 - (DslString*) setString:(NSString*)property to:(DslString*)value
 {
-  NSString *selector = [NSString stringWithFormat:@"%@:", property];
-  NSString *strVal = (NSString*)[obj performSelector:sel_getUid([selector UTF8String]) withObject:[value stringValue]];
+  [obj setValue:[value stringValue] forKey:property];
+//  NSString *selector = [NSString stringWithFormat:@"%@:", property];
+//  NSString *strVal = (NSString*)[obj performSelector:sel_getUid([selector UTF8String]) withObject:[value stringValue]];
   return value;
 }
 
 
 - (DslBoolean*) setBoolean:(NSString*)property to:(DslBoolean*)value
 {
-  NSString *selector = [NSString stringWithFormat:@"%@:", property];
-  BOOL boolVal = (BOOL)[obj performSelector:sel_getUid([selector UTF8String]) withObject:[value booleanValue]];
+  [obj setValue:[value booleanValue] forKey:property];
+//  NSString *selector = [NSString stringWithFormat:@"%@:", property];
+//  BOOL boolVal = (BOOL)[obj performSelector:sel_getUid([selector UTF8String]) withObject:[value booleanValue]];
   return value;
 }
 
